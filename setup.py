99ea529a33e85f714f36f6b0c40950fd73b2c2bb
Soura <souradeep.ghosh.13@gmail.com>
2026-06-03 19:25:40 +0530

    Add all requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Souradeep Ghosh',
+    author_email='i.am.souradeepghosh@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    
+)   
+    
